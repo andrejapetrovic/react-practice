8a0afc6fffffb0144ac6503cf44ce7739d539f6b
andrejapetrovic <andreja2092@gmail.com>
2020-08-28 21:24:51 +0200

    components, state, handlers

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Movie from './Conmponents/Movie';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+	  	<Movie />
+      </header>
+    </div>
+  );
+}
+
+export default App;
