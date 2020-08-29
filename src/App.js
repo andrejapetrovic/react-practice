81ff8dab64dd8d4b3572d9ed793ecee47e6d8d3b
andrejapetrovic <andreja2092@gmail.com>
2020-08-29 22:11:35 +0200

    Routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './App.css';
 import Movie from './Components/Movie';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-	  	<Movie />
-      </header>
-    </div>
+		<Router>
+			<main>
+				<nav>
+					<ul>
+						<li><a href="/">Home</a></li>
+						<li><Link to="/new">New Movie</Link></li>
+					</ul>
+					</nav>
+
+				<div className="App">
+					<header className="App-header">
+						<Route path="/new" component={Movie} />
+					</header>
+				</div>
+
+			 </main>
+			</Router>
   );
 }
 
--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -2,21 +2,35 @@ import React from 'react';
 
 export default class Movie extends React.Component {
 	state = {
-		title: 'Title'
+		title: 'Title',
+		genre: 'Genre',
 	}
 
 	changeHandler = (event) => {
 		event.preventDefault();
-		console.log(event.target.title);
-		this.setState({title: event.target.title.value});
+		this.setState({
+			title: event.target.title.value,
+			genre: event.target.genre.value
+		});
+		console.log(this.state);
 	}
 
 	render() {
 		return (
 			<div>
 				{this.state.title}
+				<br />
+				{this.state.genre}
 			<form onSubmit={this.changeHandler}>
 				<input type="text" defaultValue={this.state.title} name="title" />
+				<br />
+				<label>Zanr </label>
+				<select name="genre">
+					<option>Komedija</option>
+					<option>Akcioni</option>
+					<option>Triler</option>
+				</select>
+				<br />
 				<input type="submit" value="Submit" />
 			</form>
 			</div>
